55dd3af21b8e5a32ad2686b4ac1327677cbb1ec6
Phúc Lê <lthanhphuc99@gmail.com>
2022-08-23 13:53:37 +0700

    Thêm Module

---
+++ Module/quanlykhachhang/Functions.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Module\quanlysanpham;
+
+class Functions {
+    //put your code here
+    
+}
---
+++ Module/quanlythuoc/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Module\quanlythuoc;
+
+class Permission {
+    //put your code here
+    const QuanLyThuoc = "QuanLyThuoc";
+
+    public function __construct() {
+        
+    }
+    
+}
