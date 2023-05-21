cc875960a135c0a6f93f6ec35a1ec37bfa244494
namdongvando <80659514+donvbakcocomvn@users.noreply.github.com>
2023-05-21 11:40:09 +0700

    Thêm nút

--- Module/quanlythuoc/Model/btnHtml.php
+++ Module/quanlythuoc/Model/btnHtml.php
@@ -18,10 +18,11 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Thuoc_Import]) == false) {
             return;
         }
-?>
-        <a class="btn btn-warning" <?php echo FormRender::ToolTip("Import danh sách thuốc từ File Excel"); ?> href="/index.php?module=quanlythuoc&controller=sanpham&action=import">
+        ?>
+        <a class="btn btn-warning" <?php echo FormRender::ToolTip("Import danh sách thuốc từ File Excel"); ?>
+            href="/index.php?module=quanlythuoc&controller=sanpham&action=import">
             <i class="fa fa-download"></i> Import</a>
-    <?php
+        <?php
     }
 
     static function btnExportSanPham()
@@ -29,10 +30,11 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Thuoc_Export]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-success" <?php echo FormRender::ToolTip("Export danh sách thuốc ra Excel"); ?> href="/index.php?module=quanlythuoc&controller=sanpham&action=export">
+        ?>
+        <a class="btn btn-success" <?php echo FormRender::ToolTip("Export danh sách thuốc ra Excel"); ?>
+            href="/index.php?module=quanlythuoc&controller=sanpham&action=export">
             <i class="fa fa-filter"></i> Export</a>
-    <?php
+        <?php
     }
 
     static function btnThemSanPham()
@@ -40,10 +42,11 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Thuoc_Post]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-info" <?php echo FormRender::ToolTip("Thêm thuốc mới vào kho"); ?> href="/index.php?module=quanlythuoc&controller=sanpham&action=post">
+        ?>
+        <a class="btn btn-info" <?php echo FormRender::ToolTip("Thêm thuốc mới vào kho"); ?>
+            href="/index.php?module=quanlythuoc&controller=sanpham&action=post">
             <i class="fa fa-plus"></i> Thêm mới</a>
-    <?php
+        <?php
     }
 
     public static function btnViewSanPham()
@@ -51,10 +54,10 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Thuoc_DS]) == false) {
             return;
         }
-    ?>
+        ?>
         <a class="btn btn-sm btn-success" href="/index.php?module=quanlythuoc&controller=sanpham&action=index">
             <i class="fa fa-list"></i> Xem danh sách thuốc</a>
-    <?php
+        <?php
     }
 
     public static function btnSuaSanPham($id)
@@ -62,11 +65,12 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Thuoc_Put]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-default" <?php echo FormRender::ToolTip("Sửa thuốc"); ?> href="/index.php?module=quanlythuoc&controller=sanpham&action=put&id=<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-default" <?php echo FormRender::ToolTip("Sửa thuốc"); ?>
+            href="/index.php?module=quanlythuoc&controller=sanpham&action=put&id=<?php echo $id; ?>">
             <i class="fa fa-pencil text-primary"></i>
         </a>
-    <?php
+        <?php
     }
 
     public static function btnDongBoSL()
@@ -74,11 +78,11 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy]) == false) {
             return;
         }
-    ?>
+        ?>
         <a class="btn btn-primary" href="/index.php?module=quanlythuoc&controller=sanpham&action=dongboSL">
             Đồng bộ số lượng
         </a>
-    <?php
+        <?php
     }
 
     public static function btnChiTietSanPham($id)
@@ -86,11 +90,24 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Thuoc_Detail]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xem chi tiết thuốc"); ?> href="/index.php?module=quanlythuoc&controller=sanpham&action=detail&id=<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xem chi tiết thuốc"); ?>
+            href="/index.php?module=quanlythuoc&controller=sanpham&action=detail&id=<?php echo $id; ?>">
             <i class="fa fa-eye text-yellow"></i>
         </a>
-    <?php
+        <?php
+    }
+    public static function btnBaoCao($id)
+    {
+        if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_BaoCao]) == false) {
+            return;
+        }
+        ?>
+        <a class="btn btn-default" <?php echo FormRender::ToolTip("Báo cáo thuốc"); ?>
+            href="/quanlythuoc/baocao/thuoc/<?php echo $id; ?>/">
+            <i class="fa fa-list text-yellow"></i>
+        </a>
+        <?php
     }
 
     public static function btnXoaSanPham($id)
@@ -98,11 +115,12 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Thuoc_Delete]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xóa thuốc"); ?> title="Bạn có muốn xóa danh mục này?" href="/index.php?module=quanlythuoc&controller=sanpham&action=isdelete&id=<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xóa thuốc"); ?> title="Bạn có muốn xóa danh mục này?"
+            href="/index.php?module=quanlythuoc&controller=sanpham&action=isdelete&id=<?php echo $id; ?>">
             <i class="fa fa-trash-o text-red"></i>
         </a>
-    <?php
+        <?php
     }
 
     public static function btnViewDanhMuc()
@@ -110,10 +128,10 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_DanhMuc_DS]) == false) {
             return;
         }
-    ?>
+        ?>
         <a class="btn btn-primary" href="/index.php?module=quanlythuoc&controller=danhmuc&action=index">
             <i class="fa fa-plus"></i> Xem danh mục</a>
-    <?php
+        <?php
     }
 
     public static function btnThemDanhMuc()
@@ -121,10 +139,11 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_DanhMuc_Post]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-info" <?php echo FormRender::ToolTip("Thêm danh mục thuốc mới "); ?> href="/index.php?module=quanlythuoc&controller=danhmuc&action=post">
+        ?>
+        <a class="btn btn-info" <?php echo FormRender::ToolTip("Thêm danh mục thuốc mới "); ?>
+            href="/index.php?module=quanlythuoc&controller=danhmuc&action=post">
             <i class="fa fa-plus"></i> Thêm mới</a>
-    <?php
+        <?php
     }
 
     public static function btnSuaDanhMuc($id)
@@ -132,11 +151,12 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_DanhMuc_Put]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-default" <?php echo FormRender::ToolTip("Sửa danh mục thuốc"); ?> href="/index.php?module=quanlythuoc&controller=danhmuc&action=put&id=<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-default" <?php echo FormRender::ToolTip("Sửa danh mục thuốc"); ?>
+            href="/index.php?module=quanlythuoc&controller=danhmuc&action=put&id=<?php echo $id; ?>">
             <i class="fa fa-pencil text-blue"></i>
         </a>
-    <?php
+        <?php
     }
 
     public static function btnXoaDanhMuc($id)
@@ -144,11 +164,13 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_DanhMuc_Delete]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xóa danh mục thuốc"); ?> title="Bạn có muốn xóa danh mục này?" href="/index.php?module=quanlythuoc&controller=danhmuc&action=delete&id=<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xóa danh mục thuốc"); ?>
+            title="Bạn có muốn xóa danh mục này?"
+            href="/index.php?module=quanlythuoc&controller=danhmuc&action=delete&id=<?php echo $id; ?>">
             <i class="fa fa-trash-o text-red"></i>
         </a>
-    <?php
+        <?php
     }
 
     public static function btnThemPhieuXuatNhap()
@@ -156,11 +178,12 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Phieu_Post]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-info" <?php echo FormRender::ToolTip("Thêm phiếu Xuất/Nhập mới"); ?> href="/index.php?module=quanlythuoc&controller=phieuxuatnhap&action=post&isnews=1">
+        ?>
+        <a class="btn btn-info" <?php echo FormRender::ToolTip("Thêm phiếu Xuất/Nhập mới"); ?>
+            href="/index.php?module=quanlythuoc&controller=phieuxuatnhap&action=post&isnews=1">
             <i class="fa fa-plus"></i> Thêm Phiếu Mới
         </a>
-    <?php
+        <?php
     }
 
     public static function btnSuaPhieuXuatNhap($id)
@@ -168,11 +191,12 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Phieu_Put]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Sửa phiếu" href="/index.php?module=quanlythuoc&controller=phieuxuatnhap&action=put&id=<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Sửa phiếu"
+            href="/index.php?module=quanlythuoc&controller=phieuxuatnhap&action=put&id=<?php echo $id; ?>">
             Sửa
         </a>
-    <?php
+        <?php
     }
 
     public static function btnXoaPhieuXuatNhap($id)
@@ -180,11 +204,13 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Phieu_Delete]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top" title="Xóa phiếu" title="Bạn có muốn xóa phiếu này?" href="/index.php?module=quanlythuoc&controller=phieuxuatnhap&action=isdelete&id=<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top" title="Xóa phiếu"
+            title="Bạn có muốn xóa phiếu này?"
+            href="/index.php?module=quanlythuoc&controller=phieuxuatnhap&action=isdelete&id=<?php echo $id; ?>">
             Xóa
         </a>
-    <?php
+        <?php
     }
 
     public static function btnChiTietPhieuXuatNhap($id)
@@ -192,10 +218,11 @@ class btnHtml
         if (\Model\Permission::CheckPremision([\Model\User::Admin, \Model\User::QuanLy, Permission::QLT_Phieu_Detail]) == false) {
             return;
         }
-    ?>
-        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xem chi tiết phiếu"); ?> href="/quanlythuoc/phieuxuatnhap/detail/<?php echo $id; ?>">
+        ?>
+        <a class="btn btn-default" <?php echo FormRender::ToolTip("Xem chi tiết phiếu"); ?>
+            href="/quanlythuoc/phieuxuatnhap/detail/<?php echo $id; ?>">
             <i class="fa fa-eye text-yellow"></i>
         </a>
-<?php
+        <?php
     }
 }
